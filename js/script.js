23a5dc243cb4ddeaf2665a1a185da4c7c14c9525
Roselene Gabun <roselenegabun@gmail.com>
2022-11-21 23:26:33 -0800

    Minor Code Cleanup

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-
 window.onload = function (){
 
     // Function used to shrink nav bar removing paddings and adding black background
     $(window).scroll(function() {
         if ($(document).scrollTop() > 50) {
             $('.nav').addClass('affix');
-            console.log("OK");
         } else {
             $('.nav').removeClass('affix');
         }
